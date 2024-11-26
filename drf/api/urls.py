860a7494754b3100ff263b852002a248686dfe41
miderror <miderror.dev@gmail.com>
2024-11-26 14:11:01 +0300

    Update API views and URL paths

--- drf/api/urls.py
+++ drf/api/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('leaderboard/', leaderboard_view),
-    path('task/', get_task),
-    path('answer/', answer_view),
-    path('enemy/', enemy_view),
-    path('room/', RoomView.as_view()),
+    path('leaderboard/', leaderboard_view, name='leaderboard'),
+    path('answer/', answer_view, name='answer'),
+    path('enemy/', enemy_view, name='enemy'),
+    path('room/', room_view, name='room'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
