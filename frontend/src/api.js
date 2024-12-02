f0045b39bf3442079846dd3abd955030ed5934e7
Simankov Alexandr <148748481+Fotlex@users.noreply.github.com>
2024-12-02 12:43:46 +1000

    register

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:80/api',
+    baseURL: 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json'
     }
