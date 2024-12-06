6b570786e0f9201e67b79ac093ecc6fb0f1ebdbd
miderror <miderror.dev@gmail.com>
2024-12-06 05:42:52 +0300

    Add Electron integration

---
+++ frontend/electron-main.js
@@ -0,0 +1,26 @@
+import {app, BrowserWindow, Menu} from 'electron';
+
+function createWindow() {
+    let mainWindow = new BrowserWindow({
+        width: 1100,
+        height: 900,
+        minWidth: 1100,
+        minHeight: 900,
+        title: 'MathRace',
+    });
+
+    mainWindow.loadURL('http://localhost:5173');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', () => {
+    createWindow();
+    const menu = Menu.buildFromTemplate([]);
+    Menu.setApplicationMenu(menu);
+});
+app.on('window-all-closed', () => {
+    app.quit();
+});
